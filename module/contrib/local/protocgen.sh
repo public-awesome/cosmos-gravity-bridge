e812eb683406c9e181167dab772295de49e681a1
Jack Zampolin <jack.zampolin@gmail.com>
2020-11-09 16:36:50 -0800

    Working proto generation

--- module/contrib/local/protocgen.sh
+++ module/contrib/local/protocgen.sh
@@ -21,9 +21,9 @@ Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
 done
 
 # generate codec/testdata proto code
-protoc -I "proto" -I "third_party/proto" -I "testutil/testdata" --gocosmos_out=plugins=interfacetype+grpc,\
-Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. ./testutil/testdata/*.proto
+# protoc -I "proto" -I "third_party/proto" -I "testutil/testdata" --gocosmos_out=plugins=interfacetype+grpc,\
+# Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. ./testutil/testdata/*.proto
 
 # move proto files to the right places
-cp -r github.com/cosmos/cosmos-sdk/* ./
+cp -r github.com/althea-net/peggy/module/* ./
 rm -rf github.com
